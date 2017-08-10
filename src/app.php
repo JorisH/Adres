7fd67915585222645ecc21dba283dc724ab6942b
Joris Hontelé <joris@tactics.be>
2017-08-10 12:13:48 +0200

    added CORS

--- src/app.php
+++ src/app.php
@@ -1,11 +1,12 @@
 <?php
 
 use Silex\Application;
-use Silex\Provider\AssetServiceProvider;
-use Silex\Provider\DoctrineServiceProvider;
-use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\AssetServiceProvider;
+use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
+use Silex\Provider\DoctrineServiceProvider;
+use JDesrosiers\Silex\Provider\CorsServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
@@ -20,5 +21,9 @@ $app['twig'] = $app->extend('twig', function ($twig, $app) {
 $app->register(new DoctrineServiceProvider(), array(
   'db.options' => include __DIR__.'/../config/db.php'
 ));
+$app->register(new CorsServiceProvider(), [
+  "cors.allowOrigin" => "*",
+]);
+$app["cors-enabled"]($app);
 
 return $app;
