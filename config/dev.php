b7fbc6a6d867909414cd7df97c869d3b2202c2b5
Joris Hontelé <joris@tactics.be>
2017-08-11 14:49:08 +0200

    removed unused code

--- config/dev.php
+++ config/dev.php
@@ -1,5 +1,6 @@
 <?php
 
+use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\MonologServiceProvider;
 use Silex\Provider\WebProfilerServiceProvider;
 
@@ -9,10 +10,10 @@ require __DIR__.'/prod.php';
 // enable the debug mode
 $app['debug'] = true;
 
+$app->register(new TwigServiceProvider());
 $app->register(new MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__.'/../var/logs/silex_dev.log',
 ));
-
 $app->register(new WebProfilerServiceProvider(), array(
     'profiler.cache_dir' => __DIR__.'/../var/cache/profiler',
 ));
--- src/app.php
+++ src/app.php
@@ -2,22 +2,11 @@
 
 use Silex\Application;
 use Silex\Provider\ServiceControllerServiceProvider;
-use Silex\Provider\AssetServiceProvider;
-use Silex\Provider\TwigServiceProvider;
-use Silex\Provider\HttpFragmentServiceProvider;
 use Silex\Provider\DoctrineServiceProvider;
 use JDesrosiers\Silex\Provider\CorsServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
-$app->register(new AssetServiceProvider());
-$app->register(new TwigServiceProvider());
-$app->register(new HttpFragmentServiceProvider());
-$app['twig'] = $app->extend('twig', function ($twig, $app) {
-    // add custom globals, filters, tags, ...
-
-    return $twig;
-});
 $app->register(new DoctrineServiceProvider(), array(
   'db.options' => include __DIR__.'/../config/db.php'
 ));
